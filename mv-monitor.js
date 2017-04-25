9adae029d8db969adb08a058ce172bb8cd04fba1
arielcessario <arielcessario@gmail.com>
2017-04-25 17:36:33 -0300

    Navegación
    Navegación por teclado

--- mv-monitor.js
+++ mv-monitor.js
@@ -14,12 +14,14 @@
         }
     }
 
-    MvMonitorController.$inject = ["ComandasService", "ComandasVars", "MvUtils", "MesasService", "EnviosService", "$location", "ComandaService"];
+    MvMonitorController.$inject = ["ComandasService", "$timeout", "$document", "MesasService", "$element",
+        "$location", "ComandaService", '$interval'];
     /**
      * @param mvMonitor
      * @constructor
      */
-    function MvMonitorController(ComandasService, ComandasVars, MvUtils, MesasService, EnviosService, $location, ComandaService) {
+    function MvMonitorController(ComandasService, $timeout, $document, MesasService, $element,
+                                 $location, ComandaService, $interval) {
         var vm = this;
 
         vm.comandas = [];
@@ -32,30 +34,80 @@
         vm.cancelarPlato = cancelarPlato;
         vm.confirmarElaboracion = confirmarElaboracion;
         vm.showCaja = showCaja;
+        vm.navigate = navigate;
 
 
-        MesasService.get().then(function(mesas){
+        MesasService.get().then(function (mesas) {
             vm.mesas = mesas;
-        }).catch(function(error){
+        }).catch(function (error) {
             console.log(error);
         });
 
-        loadComandas();
+        // loadComandas();
+
+
+        // NAVEGACIÓN
+
+        vm.comandaSelected = 0;
+        vm.comandasAmount = 0;
+        vm.columns = 0;
+        vm.rows = 0;
+        vm.activeDish = -1;
+        vm.activeDetalle = '';
+        vm.active = false;
+
+
+        calculateRowsColums();
+        vm.el = angular.element(document.querySelector('#mv-monitor-container'));
+        vm.el[0].focus();
+        $interval(function () {
+            calculateRowsColums();
+        }, 10000);
+
+
+        function calculateRowsColums() {
+            if (vm.comanda.length == 0) {
+                return;
+            }
+            ComandasService.getByParams("status", "0,1,2,3", "true").then(function (comandas) {
+                //console.log(comandas);
+                vm.comandas = comandas;
+                // var comandas = angular.element(document.querySelector('.container-comanda'));
+
+                $timeout(function () {
+                    vm.el[0].focus();
+                    var _comandas = vm.el[0].getElementsByClassName('container-comanda');
+                    var boundingRect = vm.el[0].getBoundingClientRect();
+                    var comandaBoundingRect = _comandas[0].getBoundingClientRect();
+
+                    vm.columns = Math.floor(boundingRect.width / (comandaBoundingRect.width * 1.1));
+                    vm.rows = (_comandas.length % vm.columns == 2) ? vm.comandas.length / vm.columns : Math.ceil(_comandas.length / vm.columns);
+                }, 0);
+
+
+            }).catch(function (error) {
+                console.log(error);
+            });
+
+
+        }
+
 
         function loadComandas() {
             ComandasService.getByParams("status", "0,1,2,3", "true").then(function (comandas) {
                 //console.log(comandas);
                 vm.comandas = comandas;
-            }).catch(function(error){
+                calculateRowsColums();
+            }).catch(function (error) {
                 console.log(error);
             });
         }
 
         function getOrigen(origen_id) {
             var origen = '';
-            if(origen_id == -1) {
+            if (origen_id == -1) {
                 origen = 'Mostrador';
-            } else if(origen_id == -2) {
+            } else if (origen_id == -2) {
                 origen = 'Delivery';
             } else {
                 origen = 'Mesa ' + getMesa(origen_id);
@@ -65,17 +117,17 @@
         }
 
         function getMesa(origen_id) {
-            for(var i=0; i <= vm.mesas.length - 1; i++) {
-                if(origen_id == vm.mesas[i].mesa_id) {
+            for (var i = 0; i <= vm.mesas.length - 1; i++) {
+                if (origen_id == vm.mesas[i].mesa_id) {
                     return vm.mesas[i].mesa;
                 }
             }
         }
 
         function getNroEnvio(comanda) {
-            if(comanda.origen_id == -2) {
+            if (comanda.origen_id == -2) {
                 return '#' + comanda.envio_id;
-            } else if(comanda.origen_id == -1) {
+            } else if (comanda.origen_id == -1) {
                 return '#' + comanda.comanda_id;
             }
         }
@@ -83,10 +135,10 @@
 
         function cancelarPlato(comanda_detalle_id) {
             //console.log(comanda_detalle_id);
-            ComandasService.quitar(comanda_detalle_id).then(function(data){
+            ComandasService.quitar(comanda_detalle_id).then(function (data) {
                 //console.log(data);
                 loadComandas();
-            }).catch(function(error){
+            }).catch(function (error) {
                 console.log(error);
             });
         }
@@ -94,12 +146,11 @@
         function confirmarElaboracion(detalle) {
             detalle.status = 2;
             detalle.platoStatus = 2;
-            detalle.preparacion_fin = new Date();
             //console.log(detalle);
-            ComandasService.confirmarElaboracion(detalle).then(function(data){
+            ComandasService.confirmarElaboracion(detalle).then(function (data) {
                 //console.log(data);
                 loadComandas();
-            }).catch(function(error){
+            }).catch(function (error) {
                 console.log(error);
             });
         }
@@ -111,36 +162,86 @@
             $location.path('/caja/cobros');
         }
 
-        // Implementación de la paginación
-        vm.start = 0;
-        vm.limit = ComandasVars.paginacion;
-        vm.pagina = ComandasVars.pagina;
-        vm.paginas = ComandasVars.paginas;
 
-        function paginar(vars) {
-            if (vars == {}) {
+        function navigate(event) {
+
+            // Izquierda
+            if (event.keyCode == 37) {
+                vm.comandaSelected = (vm.comandaSelected <= 0) ? 0 : vm.comandaSelected - 1;
                 return;
             }
-            vm.start = vars.start;
-            vm.pagina = vars.pagina;
+            // Arriba
+            if (event.keyCode == 38) {
+                if (vm.active) {
+                    vm.activeDish = (vm.activeDish - 1 < 0) ? 0 : vm.activeDish - 1;
+                    vm.activeDetalle = 'detalle-' + vm.comandaSelected + '-' + vm.activeDish;
+                } else {
+                    vm.comandaSelected = (vm.comandaSelected - vm.columns < 0) ? 0 : vm.comandaSelected - vm.columns;
+                }
+                scrollPosition();
+                return;
+                // vm.el[0].scrollTop = angular.element(document.querySelector('#comanda-' + vm.comandaSelected)).getBoundingClientRect().top;
+            }
+            // Derecha
+            if (event.keyCode == 39) {
+                vm.comandaSelected = (vm.comandaSelected >= vm.comandas.length) ? vm.comandas.length - 1 : vm.comandaSelected + 1;
+                return;
+            }
+            // Abajo
+            if (event.keyCode == 40) {
+                if (vm.active) {
+                    vm.activeDish = (vm.activeDish + 1 > Object.getOwnPropertyNames(vm.comandas[vm.comandaSelected].detalles).length - 1) ? Object.getOwnPropertyNames(vm.comandas[vm.comandaSelected].detalles).length - 1 : vm.activeDish + 1;
+                    vm.activeDetalle = 'detalle-' + vm.comandaSelected + '-' + vm.activeDish;
+                } else {
+                    vm.comandaSelected = (vm.comandaSelected + vm.columns > vm.comandas.length - 1) ? vm.comandas.length - 1 : vm.comandaSelected + vm.columns;
+                }
+                scrollPosition();
+                return;
+            }
+
+            // +
+            if (event.keyCode == 107) {
+                vm.active = true;
+                vm.activeDish = 0;
+                vm.activeDetalle = 'detalle-' + vm.comandaSelected + '-' + vm.activeDish;
+                return;
+            }
+
+            // - o esc
+            if (event.keyCode == 109 || event.keyCode == 27) {
+                vm.active = false;
+                vm.activeDish = -1;
+                vm.activeDetalle = '';
+                return;
+            }
+
+            // Enter
+            if (event.keyCode == 13) {
+                if (vm.active) {
+                    confirmarElaboracion(vm.comandas[vm.comandaSelected].detalles[Object.getOwnPropertyNames(vm.comandas[vm.comandaSelected].detalles)[vm.activeDish]]);
+                } else {
+                    showCaja(vm.comandas[vm.comandaSelected]);
+                }
+                return;
+
+            }
+
+            // / - Cancelar Pedido
+            if (event.keyCode == 11) {
+                if (vm.active) {
+                    cancelarPlato(vm.comandas[vm.comandaSelected].detalles[Object.getOwnPropertyNames(vm.comandas[vm.comandaSelected].detalles)[vm.activeDish]].comanda_detalle_id);
+                }
+                return;
+            }
+
         }
 
-        vm.next = function () {
-            paginar(MvUtils.next(ComandasVars));
-        };
-        vm.prev = function () {
-            paginar(MvUtils.prev(ComandasVars));
-        };
-        vm.first = function () {
-            paginar(MvUtils.first(ComandasVars));
-        };
-        vm.last = function () {
-            paginar(MvUtils.last(ComandasVars));
-        };
-
-        vm.goToPagina = function () {
-            paginar(MvUtils.goToPagina(vm.pagina, ComandasVars));
+        function scrollPosition() {
+            var row = Math.ceil((vm.comandaSelected + 1) / vm.columns);
+            var steps = $document[0].body.scrollHeight / vm.rows;
+            $document[0].body.scrollTop = steps * (row - 1);
         }
+
 
     }
 
